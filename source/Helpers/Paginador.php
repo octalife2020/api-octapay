7501a1ec1acbd9d013e358b5262b688d57a72271
Octa Life <60751672+octalife2020@users.noreply.github.com>
2021-03-03 14:07:06 -0300

    ajustes

--- source/Helpers/Paginador.php
+++ source/Helpers/Paginador.php
@@ -16,39 +16,4 @@ class Paginador
 
         return $page;
     }
-
-    public function paginas($total, $pagina, $link, $uri = null)
-    {
-        if($total > 1) {
-            $max_links = 10;
-            $links_laterais = ceil($max_links / 2);
- 
-            // variáveis para o loop
-            $inicio = $pagina - $links_laterais;
-            $limite = $pagina + $links_laterais;
-
-            echo "<nav class='navegacao'>";
-            if($pagina > 1){
-                echo "<a class='inativo paginas' href='{$link}1?$uri'> << </a>";
-                echo "<a class='inativo paginas' href='$link".($pagina - 1)."'> < </a>";
-            }
-
-            for($i = $inicio; $i <= $limite; $i++){
-                if($pagina == $i){
-                    $class = "ativo";
-                }
-                else {
-                    $class = "inativo";
-                }
-                if ($i >= 1 && $i <= $total){
-                    echo "<a class='$class paginas' href='$link$i?$uri'>$i</a>";
-                }
-            }
-            if($pagina < $total){
-                echo "<a class='inativo paginas' href='$link".($pagina + 1)."'> > </a>";
-                echo "<a class='inativo paginas' href='$link$total?$uri'> >> </a>";
-            }
-            echo "</nav>";
-        }
-    }
 }
